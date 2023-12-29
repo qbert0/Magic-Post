5f2401a26157a48a21ec8b0dc532378633dad500
k.m <zzz@gmail.com>
2023-12-29 22:43:01 +0700

    change font

--- app/(organization)/layout.tsx
+++ app/(organization)/layout.tsx
@@ -1,50 +1,44 @@
 import { ClerkProvider, currentUser } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "../globals.css"
+import { Nunito } from "next/font/google";
+import "../globals.css";
 import { fetchUser } from "@/lib/actions/user.action";
 import Provider from "../(Provider)/NextUiProvider";
 
-
-
-export const metadata : Metadata = {
-    title: 'Magic Post',
-    description: 'A Next.js 13 Meta Threads Application',
-}
+export const metadata: Metadata = {
+  title: "Magic Post",
+  description: "A Next.js 13 Meta Threads Application",
+};
 
 interface Props {
-    children : React.ReactNode;
+  children: React.ReactNode;
 }
 
-const inter = Inter({subsets:["latin"]})
+const nunito = Nunito({ weight: "400", subsets: ["latin"] });
 
+async function RootLayout({ children }: Props) {
+  const user = await currentUser();
+  if (!user) return;
 
-async function RootLayout({children}: Props) {
-    const user = await currentUser()
-    if(!user) return
-
-    const userInfor = await fetchUser(user.id)
-    // if(!userInfor.isPostion) {
-    //     return(
-    //         <>
-    //         trang khong huu hien
-    //         </>
-    //     )
-    // }
-    return (
-        <>
-        <ClerkProvider>
-            {/* <Provider> */}
-            <html lang="en">
-                <body>
-                    {children}
-                </body>
-            </html>
-            {/* </Provider> */}
-        </ClerkProvider>
-        </>
-    )
+  const userInfor = await fetchUser(user.id);
+  // if(!userInfor.isPostion) {
+  //     return(
+  //         <>
+  //         trang khong huu hien
+  //         </>
+  //     )
+  // }
+  return (
+    <>
+      <ClerkProvider>
+        {/* <Provider> */}
+        <html lang="en">
+          <body>{children}</body>
+        </html>
+        {/* </Provider> */}
+      </ClerkProvider>
+    </>
+  );
 }
-
 
 export default RootLayout;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,57 +1,62 @@
 import { ClerkProvider, currentUser } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "../globals.css"
+import { Nunito } from "next/font/google";
+import "../globals.css";
 import Topbar from "@/components/Shared/Topbar";
 import { Children } from "react";
-import { LeftbarLinkCustomer, RightbarLinkCustomer } from "@/client/contants/sidebarlink";
+import {
+  LeftbarLinkCustomer,
+  RightbarLinkCustomer,
+} from "@/client/contants/sidebarlink";
 import Leftbar from "@/components/Shared/Leftbar";
 import Rightbar from "@/components/Shared/Rightbar";
 import Provider from "../(Provider)/NextUiProvider";
 import { fetchUser } from "@/lib/actions/user.action";
 import { PathRoot } from "@/client/contants/enum";
 
-
-
-export const metadata : Metadata = {
-    title: 'Magic Post',
-    description: 'A Next.js 13 Meta Threads Application',
-}
+export const metadata: Metadata = {
+  title: "Magic Post",
+  description: "A Next.js 13 Meta Threads Application",
+};
 
 interface Props {
-    children : React.ReactNode;
+  children: React.ReactNode;
 }
 
-const inter = Inter({subsets:["latin"]})
+const nunito = Nunito({ weight: "400", subsets: ["latin"] });
 
+async function RootLayout({ children }: Props) {
+  const user = await currentUser();
+  if (!user) return;
+  const userInfor = await fetchUser(user.id);
+  const career = userInfor.career;
 
-async function RootLayout({children}: Props) {
-    const user = await currentUser()
-    if(!user) return
-    const userInfor = await fetchUser(user.id)
-    const career = userInfor.career
-
-    return (
-        <>
-        <ClerkProvider>
-            <html lang="en"><Provider>
-                <body className={`${inter.className} `}>
-                    <Topbar career={career} pathname={PathRoot.user}/>
-                    <main className="flex flex-row">
-                        <Leftbar sidebarlink={LeftbarLinkCustomer} pathnameRoot={PathRoot.user} />
-                        <section className='main-container '>
-                            <div className='w-full '>
-                                {children}
-                            </div>
-                        </section>
-                        <Rightbar sidebarlink={RightbarLinkCustomer} pathnameRoot={PathRoot.user}/>
-                    </main>
-                </body></Provider>
-            </html>
-        </ClerkProvider>
-        </>
-    )
+  return (
+    <>
+      <ClerkProvider>
+        <html lang="en">
+          <Provider>
+            <body className={`${nunito.className} `}>
+              <Topbar career={career} pathname={PathRoot.user} />
+              <main className="flex flex-row">
+                <Leftbar
+                  sidebarlink={LeftbarLinkCustomer}
+                  pathnameRoot={PathRoot.user}
+                />
+                <section className="main-container ">
+                  <div className="w-full ">{children}</div>
+                </section>
+                <Rightbar
+                  sidebarlink={RightbarLinkCustomer}
+                  pathnameRoot={PathRoot.user}
+                />
+              </main>
+            </body>
+          </Provider>
+        </html>
+      </ClerkProvider>
+    </>
+  );
 }
-
 
 export default RootLayout;
